1061cc52a00cd9aacb0b1edbc090fa62eab050a4
brightkams <ohakambright@gmail.com>
2023-05-30 21:16:12 +0100

    commit for consitionals in Python

---
+++ conditionals.py
@@ -0,0 +1,13 @@
+'''
+    If conditions are used to test for partiular conditions and respond appropriately.
+'''
+# Conditional tests
+
+# cycle = "bikes"
+
+if "o" in "bikes":
+    print("letter \"i\" is in bikes")
+elif "o" not in "bikes":
+    print("letter \"i\" is not in bikes")
+else:
+    print("Hello")
