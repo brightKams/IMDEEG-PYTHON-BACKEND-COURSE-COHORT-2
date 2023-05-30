f01c8bd04839696fe2c2cb104c53857fd1ec53bc
brightkams <ohakambright@gmail.com>
2023-05-30 21:18:21 +0100

    Commit for tuples squence data-type in Python

---
+++ tuples.py
@@ -0,0 +1,21 @@
+'''
+    TUPLES:
+    Tuples are similar to lists but the items are not mutable 
+    Example:
+'''
+tupleItems = ("Bright", "Ohakam")
+
+iteration = 0
+for name in range(len(tupleItems)):
+    print( str(iteration) + ":" + tupleItems[iteration])
+    iteration = +1
+print(type(iteration))
+
+
+literalComment = '''newRange = range(8)
+                    for item in newRange:
+                        print(item)'''
+print(literalComment)
+
+
+ 
