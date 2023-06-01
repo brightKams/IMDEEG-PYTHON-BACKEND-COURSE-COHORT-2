8aff5638124bcf85a006f042b25ca05e5ca9e338
brightkams <ohakambright@gmail.com>
2023-06-01 15:41:20 +0100

    commit for list methods in Python

---
+++ python_list_methods.py
@@ -0,0 +1,31 @@
+'''
+1. append() : Adds an element at the ende of a list
+2. clear() : Removes all the elements from the list
+3. copy() : Returns a copy of the list
+4. count() : Returns the number of times a particular item occurs in a list
+5. extend() : Adds the elements of a list (or any iterable), to the end of the current list
+6. index() : Returns the index of the first element with the specified value
+
+7. insert() : This adds an element at a specified position in a list
+
+8. pop() : Removes the element at the specified position
+
+9. remove() : Removes the item with the specified position
+
+10. reverse() : Reverses the order of a list
+
+11. sort() : Sorts the list in alphabetical order
+'''
+
+# count() method Example:
+counts = [1,2,3,4,5,1,5,"Ohakam"]
+print(counts.count(5))
+
+# index() method Example:
+print(counts.index("Ohakam"))
+# print(counts)
+
+
+
+
+
