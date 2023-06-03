69e6f05ebf2b826f3127197de4006834abfe14d0
brightkams <ohakambright@gmail.com>
2023-06-03 04:38:26 +0100

    commit for general data collection objects in Python

---
+++ python_collections.py
@@ -0,0 +1,17 @@
+# PYTHON COLLECTIONS (ARRAYS)
+
+'''
+    There are four collection datat types in the Python programming language:
+
+'''
+# a. List: is a a collection which is ordered and changeable. It allows duplicate members.
+
+# b. Tuple: is collection which is ordered and unchangeable. I allows duplicated members.
+
+# c. Set: is  a collection which is unordered, unchangeable, and unindexed. No duplicate member is allowed. But, it allows the removal and adding of items whenever you like.
+
+# d. Dictionary: is a collection which is ordered and changeable. No duplicate is allowed in it.
+
+# NB: in Python version 3.6 qnd earlier, dictionaries are unordered.
+
+
